a6562de626ea75756dbe3c246895e66241ad4b7a
bsimkins11-source <bsimkins11@gmail.com>
2025-08-10 21:19:35 -0500

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
 import Layout from './components/Layout'
 import HomePage from './pages/HomePage'
 import AgentLibraryPage from './pages/AgentLibraryPage'
@@ -10,21 +11,23 @@ import CreatorPortal from './pages/CreatorPortal'
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<div className="p-8 text-center">Login page coming soon...</div>} />
-          <Route path="/agents" element={<AgentLibraryPage />} />
-          <Route path="/my-agents" element={<MyAgentsPage />} />
-          <Route path="/company-admin" element={<CompanyAdminDashboard />} />
-          <Route path="/super-admin" element={<SuperAdminDashboard />} />
-          <Route path="/creator-portal" element={<CreatorPortal />} />
-          <Route path="/company/:companyId" element={<div className="p-8 text-center">Company Library page coming soon...</div>} />
-          <Route path="*" element={<div className="p-8 text-center">Page not found</div>} />
-        </Routes>
-      </Layout>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<div className="p-8 text-center">Login page coming soon...</div>} />
+            <Route path="/agents" element={<AgentLibraryPage />} />
+            <Route path="/my-agents" element={<MyAgentsPage />} />
+            <Route path="/company-admin" element={<CompanyAdminDashboard />} />
+            <Route path="/super-admin" element={<SuperAdminDashboard />} />
+            <Route path="/creator-portal" element={<CreatorPortal />} />
+            <Route path="/company/:companyId" element={<div className="p-8 text-center">Company Library page coming soon...</div>} />
+            <Route path="*" element={<div className="p-8 text-center">Page not found</div>} />
+          </Routes>
+        </Layout>
+      </Router>
+    </AuthProvider>
   )
 }
 
